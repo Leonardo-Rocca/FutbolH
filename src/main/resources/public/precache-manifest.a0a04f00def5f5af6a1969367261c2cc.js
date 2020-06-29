5dcbfc3307f5269a2ed82b0b0c648c210be2a8c4
Leonardo-Rocca <leonardo_7@live.com.ar>
2020-06-29 00:36:08 -0300

    move index

---
+++ src/main/resources/public/precache-manifest.a0a04f00def5f5af6a1969367261c2cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0473a960e299c6bf2bde1d1feb5de8b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6eb2f3f0b8be6b42cf8",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "e174c08c1988f2126222",
+    "url": "/static/js/2.6891e5e2.chunk.js"
+  },
+  {
+    "revision": "f6eb2f3f0b8be6b42cf8",
+    "url": "/static/js/main.30b9d4f0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
