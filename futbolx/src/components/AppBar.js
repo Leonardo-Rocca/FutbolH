5987b9512f4c05965ea45213f36ee344f130f4db
Leonardo-Rocca <leonardo_7@live.com.ar>
2020-06-29 00:07:33 -0300

    remove unused imports

--- futbolx/src/components/AppBar.js
+++ futbolx/src/components/AppBar.js
@@ -1,20 +1,14 @@
 import React, {useState} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import FacebookLoginButton from "./FacebookLoginButton";
 import FacebookLogin from 'react-facebook-login';
-import Delete from "@material-ui/core/SvgIcon/SvgIcon";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import {Person} from "@material-ui/icons";
 import Modal from "@material-ui/core/Modal";
-import FacebookIcon from '@material-ui/icons/Facebook';
 import {Box, Container, Paper} from "@material-ui/core";
 import Button from "@material-ui/core/Button";
-import Popper from "@material-ui/core/Popper";
 import Tooltip from "@material-ui/core/Tooltip";
 
 const useStyles = makeStyles(theme => ({
